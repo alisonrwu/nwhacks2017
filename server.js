6472fc5f0447bfd6946958296914c68e315f93f4
Trevin Wong <trevinwong@gmail.com>
2017-03-19 06:13:10 -0700

    sketchy cors

--- server.js
+++ server.js
@@ -8,29 +8,33 @@ app.use(bodyParser.urlencoded({
 
 var posts = require('./routes/posts');
 var map = require('./routes/map');
+var cors = require('cors');
+
+// cors stuff
+app.use(cors({origin: null}));
 
 
 var PORT = 8080;
 
 // Add headers
-app.use(function (req, res, next) {
+//app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Content-Length, Authorization');
+//    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Content-Length, Authorization');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
-    next();
-});
+//    next();
+//});
 
 app.use('/posts', posts);
 app.use('/map', map);
