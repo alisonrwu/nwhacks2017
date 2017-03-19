9d87633b42440538ea194402cc1d9b182eaea2e9
Bryan Ng <bryan.yc.ng@shaw.ca>
2017-03-18 18:46:34 -0700

    Add post and get for posts
    not functional right now, template for when we get database

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,67 @@
 var express = require('express');
 var router = express.Router();
-
+/*
 router.get('/', function(req, res) {
     res.send('GET handler for /posts route.');
 });
 
 router.post('/', function(req, res) {
     res.send('POST handler for /posts route.');
+});
+*/
+
+router.post('/', (req, res, next) => {
+  const results = [];
+  // Grab data from http request
+  const data = {text: req.body.text, complete: false};
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Insert Data
+    //longitude, latitude instead of location object easier?
+    client.query('INSERT INTO posts(title, text,) values($1,$2,$3)',
+    [data.title, data.text, username]);
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM posts ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+router.get('/post', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM posts ORDER BY id ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
 });
 
 module.exports = router;
