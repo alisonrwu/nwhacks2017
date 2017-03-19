f5d782fdba8fe7aaa7cdd35b9303d1b6a7149f7e
Bryan Ng <bryan.yc.ng@shaw.ca>
2017-03-18 20:08:40 -0700

    Update route post slightly

--- routes/posts.js
+++ routes/posts.js
@@ -13,7 +13,8 @@ router.post('/', function(req, res) {
 router.post('/', (req, res, next) => {
   const results = [];
   // Grab data from http request
-  const data = {text: req.body.text, complete: false};
+  const data = {content: req.body.content, lat: req.body.lat,long: req.body.long, 
+  				maxRadius: req.body.maxRadius, maxLife: req.body.maxLife,};
   // Get a Postgres client from the connection pool
   pg.connect(connectionString, (err, client, done) => {
     // Handle connection errors
@@ -24,8 +25,12 @@ router.post('/', (req, res, next) => {
     }
     // SQL Query > Insert Data
     //longitude, latitude instead of location object easier?
-    client.query('INSERT INTO posts(title, text,) values($1,$2,$3)',
-    [data.title, data.text, username]);
+
+    //get DateTime
+    var timestamp = new Date().toLocaleString();
+
+    client.query('INSERT INTO posts(timestamp,context,lat,lon,maxRadius,maxLife) values($1,$2,$3,$4,$5,$6)',
+    [timestamp, data.content, data.lat, data.long, data.maxRadius, maxLife]);
     // SQL Query > Select Data
     const query = client.query('SELECT * FROM posts ORDER BY id ASC');
     // Stream results back one row at a time
